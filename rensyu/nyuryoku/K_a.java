7b14d952179a02429421bcca2ade90d1a6ce8896
user <ktq2390307@stu.o-hara.ac.jp>
2024-01-19 11:38:16 +0900

    Commit

---
+++ rensyu/nyuryoku/K_a.java
@@ -0,0 +1,37 @@
+package rensyu.nyuryoku;
+import java.util.Scanner;
+// メインの機能
+class K_a {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        // 半径を入力
+        System.out.print("半径を整数値を入力：");
+        // 変数にクラスを入れ引き出してそれぞれの数値を代入
+        Circle a =new Circle(stdIn.nextInt(), 3.1415);
+        // Circleクラスのprinterを持ってくる
+        a.printer();
+    }
+}
+// クラスの制作
+class Circle {
+    // それぞれの型の指定
+    int x;
+    double a;
+    double b;
+    double c;
+    // 二つの数値を使えるようにする
+    Circle(int x, double a){
+        this.x = x;
+        this.a = a;
+    }
+    public void printer(){
+        // 円周と円の面積の公式を当てはめる
+        b = x*2*a;
+        c = x*x*a;
+        // 表示させる
+        System.out.println("円周の長さは"+b+"です。");
+        System.out.println("円の面積は"+c+"です。");
+
+    }
+
+}
--- rensyu/nyuryoku/K_b.java
+++ rensyu/nyuryoku/K_b.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class K_b {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        
+        int x = stdIn.nextInt();
+        int y = stdIn.nextInt();
+        int z = 0;
+        int b = x;
+        for (int a = 0; x<=y; x++){
+            a += x;
+            z = a;
+        }
+        System.out.println(b +"から"+ y +"までの合計値は"+ z +"です。");
     }
 }
