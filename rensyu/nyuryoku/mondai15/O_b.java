e847fba24607fbb60f757234f167fb63059ca2fb
user <ktq2390307@stu.o-hara.ac.jp>
2024-01-26 14:47:03 +0900

    Commit

--- rensyu/nyuryoku/mondai15/O_b.java
+++ rensyu/nyuryoku/mondai15/O_b.java
@@ -7,14 +7,24 @@ class O_b {
         Scanner stdIn = new Scanner(System.in);
         System.out.print("整数を入力 = ");
         try{
+            // 整数値を入力してもらい
             int a = stdIn.nextInt();
-            boolean b = a % 2 ==0;
-            if (b == true)
+            // 入力された数値が偶数なら
+            if (isEven(a))
+            // 表示する
                 System.out.println(a+"は偶数");
-            else if (b == false)
+            // 奇数なら
+            else
+                // 表示する
                 System.out.println(a+"は奇数");
+        // 入力されたものが違う型なら
         }catch (InputMismatchException e){
+            // 表示する
             System.out.println("整数と認識できません！！");
         }
+    }
+    // 奇数か偶数か判断するメソッド
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
     }
 }
