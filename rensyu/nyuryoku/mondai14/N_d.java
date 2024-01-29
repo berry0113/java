84d0a1e41919ef60f05a25a97635fec39ee97a30
user <ktq2390307@stu.o-hara.ac.jp>
2024-01-29 11:40:33 +0900

    Commit

--- rensyu/nyuryoku/mondai14/N_d.java
+++ rensyu/nyuryoku/mondai14/N_d.java
@@ -2,7 +2,16 @@ package rensyu.nyuryoku.mondai14;
 import java.util.Scanner;
 class N_d {
     public static void main(String[] args){
+        // ユーザからの入力を受け取る
         Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("1つ目の文字列を入力：");
+        String x = stdIn.next();
+        System.out.print("2つ目の文字列を入力：");
+        String y = stdIn.next();
         
+        // equalsで入力されたものを比較してあっていたら表示する
+        if (x.equals(y))
+            System.out.println("同じ文字列です。");
     }
 }
